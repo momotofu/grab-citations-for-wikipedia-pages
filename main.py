307568f0ed1197d8e46c940a54fc15589bc96783
Christopher Reece <mrkita.reece@gmail.com>
2018-11-07 19:51:15 +0900

    setup basic application

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+#!/usr/bin/env python
+
 from utils import open_and_read
+from bs4 import BeautifulSoup
+import requests, cli.app
+
+@cli.app.CommandLineApp
+def main(app):
+    filename = app.params.filename
+    hyperLinks = open_and_read(filename)
+    print(hyperLinks)
+
+main.add_param(
+    "filename",
+    help="A file of wikipedia page links separated by newlines")
 
 
+if __name__ == '__main__':
+    main.run()
