763e0899962e2e8150d6344ca5869eb5cd9061f8
Christopher Reece <mrkita.reece@gmail.com>
2018-11-08 10:10:13 +0900

    implemented new util functions

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 #!/usr/bin/env python
 
-from utils import open_and_read
-from bs4 import BeautifulSoup
-import requests, cli.app
+from utils import open_and_read, request_html_for
+from bs4 import BeautifulSoup as bSoup
+import cli.app
 
 @cli.app.CommandLineApp
 def main(app):
+    # Store hyperlink sitations as key value pairs with the link
+    # being the key.
+    hyperlinksCitations = {}
+
+    # Get hyperlinks from file and store in a list
     filename = app.params.filename
-    hyperLinks = open_and_read(filename)
-    print(hyperLinks)
+    hyperlinks = open_and_read(filename)
+
+    # iterate through hyperLinks and retrieve citations
+    for hyperlink in hyperlinks:
+        mainPageSoup = bSoup(request_html_for(hyperlink), 'html.parser')
+
+        # retrieve citation page hyperlink
+        citationPageHyperlink = mainPageSoup.find(
+            'a',
+            title='Information on how to cite this page')['href']
+
+        # retrieve citation page soup
+        citationPageSoup = bSoup(
+            request_html_for(citationPageHyperlink),
+            'html.parse')
+
+        # Map citation formats to hyperlinksCitations
+
 
 main.add_param(
     "filename",
--- utils.py
+++ utils.py
@@ -1,6 +1,23 @@
+import requests
+
+
 def open_and_read(file):
     """
     Opens a file and returns a list of the files lines.
     """
-    with open(file, 'r') as fileData:
-        return fileData.readlines()
+    try:
+        with open(file, 'r') as fileData:
+            return fileData.readlines()
+    except IOError:
+        print("Error: cant\'t find or read file")
+
+
+def get_html_from(url):
+    """
+    Gets text response from request made to the 'url'.
+    """
+    try:
+        response = requests.get(url)
+        return response.text
+    except requests.exceptions.RequestException as e:
+        print('Could not make request because of: ', e)
